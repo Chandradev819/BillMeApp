9b9ec287aa014dc621a138ba9048bb5579b7f7a0
Chandradev819 <chandradev819@gmail.com>
2025-07-17 14:01:25 +0530

    Code updated.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,15 @@
 using BlazorApp.Client;
-using Blazored.AzureStaticWebAppAuthentication.Extensions;
+using BlazorApp.Client.Authentication.Services;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, UserAuthenticationStateProvider>();
 await builder.Build().RunAsync();
